5331d51f813f45ff6442fc8b6557960efc0e990e
Pedro Velasco <pedro@pedro.com>
2015-05-13 15:56:32 +0200

    adadae

--- Arch_proj/src/Main.java
+++ Arch_proj/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 	int i = 0;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("FST");
+		System.out.println("2 version");
 	}
 
 }
