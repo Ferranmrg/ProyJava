b33193e20ead0cca9d5ad9e10d1da99e71c8a063
Pedro Velasco <pedro@pedro.com>
2015-05-14 16:30:07 +0200

    Version 1.0.0.3
    COJUNTADO CON TODAS LAS VENTANAS

--- Arch_proj/src/Login/VistaWelcomeImpl.java
+++ Arch_proj/src/Login/VistaWelcomeImpl.java
@@ -7,26 +7,22 @@ import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 import java.awt.BorderLayout;
+import java.awt.event.MouseAdapter;
+
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+
+import com.sun.glass.events.MouseEvent;
 
 public class VistaWelcomeImpl {
 
 	private JFrame frame;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					VistaWelcomeImpl window = new VistaWelcomeImpl();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private Modelo modelo;
+	private Controlador controlador;
+	private JTextField txtNick;
+	private JTextField txtNombre;
+	private JTextField txtApellido;
 
 	/**
 	 * Create the application.
@@ -44,25 +40,78 @@ public class VistaWelcomeImpl {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JTable table = new JTable();
-		table.setBounds(95, 97, 181, 115);
+		
+		final JTable table = new JTable();
+		final DefaultTableModel defTable = (DefaultTableModel)table.getModel();
+		table.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(java.awt.event.MouseEvent arg0) {
+				 int fila = table.rowAtPoint(arg0.getPoint());
+		         int columna = table.columnAtPoint(arg0.getPoint());
+		         if ((fila > -1) && (columna > -1))
+		            System.out.println(defTable.getValueAt(fila,columna));
+		
+			}
+		});
+		table.setBounds(60, 24, 307, 115);
 		frame.getContentPane().add(table);
 		table.setModel(new DefaultTableModel(
-				new Object[][]{
-						
-						
-				},
-				new String[] {
-						"Nick","Nombre","Apellido"
-				}
-				
-				));
+			new Object[][] {
+				{"NICK", "NOMBRE", "APELLIDO"},
+			},
+			new String[] {
+				"Nick", "Nombre", "Apellido"
+			}
+		) {
+			boolean[] columnEditables = new boolean[] {
+				false, false, false
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		
+		JButton btnAlta = new JButton("Alta");
+		btnAlta.setBounds(33, 215, 97, 25);
+		frame.getContentPane().add(btnAlta);
+		
+		JButton btnBaja = new JButton("Baja");
+		btnBaja.setBounds(173, 215, 97, 25);
+		frame.getContentPane().add(btnBaja);
+		
+		JButton btnModificacion = new JButton("Modificacion");
+		btnModificacion.setBounds(323, 215, 97, 25);
+		frame.getContentPane().add(btnModificacion);
+		
+		txtNick = new JTextField();
+		txtNick.setBounds(33, 177, 91, 25);
+		frame.getContentPane().add(txtNick);
+		txtNick.setColumns(10);
+		
+		txtNombre = new JTextField();
+		txtNombre.setColumns(10);
+		txtNombre.setBounds(173, 177, 97, 25);
+		frame.getContentPane().add(txtNombre);
+		
+		txtApellido = new JTextField();
+		txtApellido.setColumns(10);
+		txtApellido.setBounds(323, 177, 91, 25);
+		frame.getContentPane().add(txtApellido);
+		
+		JLabel lblNick = new JLabel("Nick");
+		lblNick.setBounds(37, 136, 93, 43);
+		frame.getContentPane().add(lblNick);
+		
+		JLabel lblNombre = new JLabel("Nombre");
+		lblNombre.setBounds(177, 136, 93, 43);
+		frame.getContentPane().add(lblNombre);
+		
+		JLabel lblApellido = new JLabel("Apellido");
+		lblApellido.setBounds(327, 136, 93, 43);
+		frame.getContentPane().add(lblApellido);
 		
 
-		
-				
 		
 		
 	}
-
+	
 }
